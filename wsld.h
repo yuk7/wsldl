09e5f206f5ed1339c12ff1593d455288ad4adde8
yuk7 <yukx00@gmail.com>
2017-11-22 11:19:40 +0900

    create wsld.h

---
+++ wsld.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2017 yuk7
+ * Author: yuk7 <yukx00@gmail.com>
+ *
+ * Released under the MIT license
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+
+#ifndef WSLD_H_
+#define WSLD_H_
+
+#endif
