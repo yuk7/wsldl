c6e406b56cd7315b2d64df83bde0c93e9fb2a2d8
Biswapriyo <31443074+Biswa96@users.noreply.github.com>
2017-11-22 19:57:45 +0530

    Update main.c

--- main.c
+++ main.c
@@ -6,12 +6,6 @@
  * http://opensource.org/licenses/mit-license.php
  */
 
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <wchar.h>
-#include <windows.h>
 #include "wsld.h"
 
 #define ARRAY_LENGTH(a) (sizeof(a)/sizeof(a[0]))
