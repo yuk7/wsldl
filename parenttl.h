e6b8ba9384307e6f25c2a7360a8f2843e9b14727
yuk7 <yukx00@gmail.com>
2020-11-25 09:17:21 +0900

    parenttl.h: detect start from wt.exe  fix #78

--- parenttl.h
+++ parenttl.h
@@ -38,7 +38,8 @@ bool isParentCmdLine()
             {
                 if ((strcmp(pe32.szExeFile, "cmd.exe") == 0) ||
                 (strcmp(pe32.szExeFile, "powershell.exe") == 0) ||
-                (strcmp(pe32.szExeFile, "wsl.exe") == 0) )
+                (strcmp(pe32.szExeFile, "wsl.exe") == 0) ||
+                (strcmp(pe32.szExeFile, "wt.exe") == 0) )
                 {
                     if(procsCnt < PROC_LIST_SIZE)
                     {
