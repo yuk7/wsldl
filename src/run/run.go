9cc8291cd72a7c18f6354ac1922c4f75f3a1ab8f
yuk7 <yukx00@gmail.com>
2021-07-11 12:31:30 +0900

    src: run: implement ExecRead function

--- src/run/run.go
+++ src/run/run.go
@@ -25,3 +25,30 @@ func Execute(name string, args []string) {
 		os.Exit(int(exitCode))
 	}
 }
+
+//ExecRead execs command and read output
+func ExecRead(name, command string) (out string, exitCode uint32, err error) {
+	stdin := syscall.Handle(0)
+	stdout := syscall.Handle(0)
+	stdintmp := syscall.Handle(0)
+	stdouttmp := syscall.Handle(0)
+	sa := syscall.SecurityAttributes{InheritHandle: 1, SecurityDescriptor: 0}
+
+	syscall.CreatePipe(&stdin, &stdintmp, &sa, 0)
+	syscall.CreatePipe(&stdout, &stdouttmp, &sa, 0)
+
+	handle, err := wslapi.WslLaunch(name, command, true, stdintmp, stdouttmp, stdouttmp)
+	syscall.WaitForSingleObject(handle, syscall.INFINITE)
+	syscall.GetExitCodeProcess(handle, &exitCode)
+	buf := make([]byte, syscall.MAX_LONG_PATH)
+	var length uint32
+
+	syscall.ReadFile(stdout, buf, &length, nil)
+
+	//[]byte -> string and cut to fit the length
+	out = string(buf)[:length]
+	if out[len(out)-1:] == "\n" {
+		out = out[:len(out)-1]
+	}
+	return
+}
