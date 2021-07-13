dc781ea439ac18e34e0ce0cb134eb8eef467e808
yuk7 <yukx00@gmail.com>
2021-07-14 03:44:15 +0900

    build.bat: add version info and rsrc -> goversioninfo Add versioninfo properties because exe files without the property are often falsely detected by security software
    Delete rc files that are no longer needed

--- build.bat
+++ build.bat
@@ -18,8 +18,18 @@ if "%~1"=="resources" (
     exit /b
 )
 if "%~1"=="icons" (
-    echo Building binary with icons
+    echo Building icon binaries
     call :icons
+    exit /b
+)
+if "%~1"=="single" (
+    echo Building binary...
+    call :singlewor
+    exit /b
+)
+if "%~1"=="singlewor" (
+    echo Building binary without resource...
+    call :singlewor
     exit /b
 )
 if "%~1"=="clean" (
@@ -35,11 +45,13 @@ exit /b
 :resources
 cd /d %~dp0
 mkdir tools >NUL 2>&1
-echo Downloading rsrc...
-curl -sSfL https://github.com/akavel/rsrc/releases/download/v0.10.2/rsrc_windows_amd64.exe -o tools\rsrc.exe
+echo Installing goversioninfo...
+go get github.com/josephspurrier/goversioninfo/cmd/goversioninfo
+set PATH="%GOPATH%\bin";%PATH%
+
 echo Compiling all resources...
 FOR /D /r %%D in ("res/*") DO (
-    tools\rsrc.exe -ico res\%%~nxD\icon.ico -o res\%%~nxD\res.syso
+    goversioninfo -icon res\%%~nxD\icon.ico -o res\%%~nxD\resource.syso src\versioninfo.json
 )
 exit /b
 
@@ -48,18 +60,24 @@ cd /d %~dp0
 echo Building wsldl with icons...
 mkdir out\icons >NUL 2>&1
 FOR /D /r %%D in ("res/*") DO (
-    copy /y res\%%~nxD\res.syso src\res.syso
+    copy /y res\%%~nxD\resource.syso src\resource.syso
     cd src
     echo go build %GO_BUILD_OPTS% -o "%~dp0\out\icons\%%~nxD.exe"
     go build %GO_BUILD_OPTS% -o "%~dp0\out\icons\%%~nxD.exe"
     cd ..
-    del /f src\res.syso
+    del /f src\resource.syso
 )
 exit /b
 
 :single
 cd /d %~dp0
 mkdir out >NUL 2>&1
+echo Installing goversioninfo...
+go get github.com/josephspurrier/goversioninfo/cmd/goversioninfo
+set PATH="%GOPATH%\bin";%PATH%
+echo Compiling resource object...
+goversioninfo -o src\resource.syso src\versioninfo.json
+:singlewor
 cd src
 echo Building default wsldl.exe...
 echo go build %GO_BUILD_OPTS% -o "%~dp0\out\wsldl.exe"
@@ -71,7 +89,8 @@ exit /b
 :clean
 FOR /D /r %%D in ("res/*") DO (
     cd /d %~dp0
-    del res\%%~nxD\res.syso
+    del res\%%~nxD\resource.syso
 )
+del src\resource.syso
 rmdir /s /q out tools
 exit /b
