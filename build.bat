fa0c4b896745620f8d3d207154654afe20d9d894
yuk7 <yukx00@gmail.com>
2021-07-14 15:38:58 +0900

    arm64 build support

--- build.bat
+++ build.bat
@@ -8,8 +8,51 @@ cd /d %~dp0
 set PATH="%GOPATH%\bin";%PATH%
 set PATH="%USERPROFILE%\go\bin";%PATH%
 
+if not defined GOBIN (
+    set GOBIN=go
+)
+
+if not defined GOARCH (
+    echo GOARCH is not defined, detecting cpu architecture...
+    if "%PROCESSOR_ARCHITECTURE%"=="x86" (
+        echo 32bit 386 processor detected
+        set GOARCH=386
+    )
+    if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+        echo 64bit amd64 processor detected
+        set GOARCH=amd64
+    )
+    if "%PROCESSOR_ARCHITECTURE%"=="ARM" (
+        echo 32bit arm processor detected
+        set GOARCH=arm
+    )
+    if "%PROCESSOR_ARCHITECTURE%"=="ARM64" (
+        echo 64bit arm64 processor detected
+        set GOARCH=arm64
+    )
+)
+
+if "%GOARCH%"=="386" (
+    echo GOARCH is 386
+    set GOVERSIONINFO_OPTS=
+)
+if "%GOARCH%"=="amd64" (
+    echo GOARCH is amd64
+    set GOVERSIONINFO_OPTS=-64
+)
+if "%GOARCH%"=="arm" (
+    echo GOARCH is arm
+    set GOVERSIONINFO_OPTS=-arm
+)
+if "%GOARCH%"=="arm64" (
+    echo GOARCH is arm64
+    set GOVERSIONINFO_OPTS=-arm -64
+)
+
+
 if "%~1"=="all" (
     echo Building everything
+    call :dlgoversioninfo
     call :resources
     call :icons
     call :single
@@ -17,6 +60,7 @@ if "%~1"=="all" (
 )
 if "%~1"=="resources" (
     echo Building resources
+    call :dlgoversioninfo
     call :resources
     exit /b
 )
@@ -27,7 +71,8 @@ if "%~1"=="icons" (
 )
 if "%~1"=="single" (
     echo Building binary...
-    call :singlewor
+    call :dlgoversioninfo
+    call :single
     exit /b
 )
 if "%~1"=="singlewor" (
@@ -40,6 +85,7 @@ if "%~1"=="clean" (
     call :clean
     exit /b
 )
+call :dlgoversioninfo
 call :single
 exit /b
 
@@ -47,13 +93,9 @@ exit /b
 
 :resources
 cd /d %~dp0
-mkdir tools >NUL 2>&1
-echo Installing goversioninfo...
-go get github.com/josephspurrier/goversioninfo/cmd/goversioninfo
-
 echo Compiling all resources...
 FOR /D /r %%D in ("res/*") DO (
-    goversioninfo -icon res\%%~nxD\icon.ico -o res\%%~nxD\resource.syso src\versioninfo.json
+    tools\goversioninfo %GOVERSIONINFO_OPTS% -icon res\%%~nxD\icon.ico -o res\%%~nxD\resource.syso src\versioninfo.json
 )
 exit /b
 
@@ -64,8 +106,8 @@ mkdir out\icons >NUL 2>&1
 FOR /D /r %%D in ("res/*") DO (
     copy /y res\%%~nxD\resource.syso src\resource.syso
     cd src
-    echo go build %GO_BUILD_OPTS% -o "%~dp0\out\icons\%%~nxD.exe"
-    go build %GO_BUILD_OPTS% -o "%~dp0\out\icons\%%~nxD.exe"
+    echo %GOBIN% build %GO_BUILD_OPTS% -o "%~dp0\out\icons\%%~nxD.exe"
+    %GOBIN% build %GO_BUILD_OPTS% -o "%~dp0\out\icons\%%~nxD.exe"
     cd ..
     del /f src\resource.syso
 )
@@ -73,17 +115,15 @@ exit /b
 
 :single
 cd /d %~dp0
-mkdir out >NUL 2>&1
-echo Installing goversioninfo...
-go get github.com/josephspurrier/goversioninfo/cmd/goversioninfo
-
 echo Compiling resource object...
-goversioninfo -o src\resource.syso src\versioninfo.json
+tools\goversioninfo %GOVERSIONINFO_OPTS% -o src\resource.syso src\versioninfo.json
 :singlewor
+cd /d %~dp0
+mkdir out >NUL 2>&1
 cd src
 echo Building default wsldl.exe...
-echo go build %GO_BUILD_OPTS% -o "%~dp0\out\wsldl.exe"
-go build %GO_BUILD_OPTS% -o "%~dp0\out\wsldl.exe"
+echo %GOBIN% build %GO_BUILD_OPTS% -o "%~dp0\out\wsldl.exe"
+%GOBIN% build %GO_BUILD_OPTS% -o "%~dp0\out\wsldl.exe"
 cd ..
 :end
 exit /b
@@ -95,4 +135,24 @@ FOR /D /r %%D in ("res/*") DO (
 )
 del src\resource.syso
 rmdir /s /q out tools
+exit /b
+
+:dlgoversioninfo
+cd /d %~dp0
+mkdir tools >NUL 2>&1
+if "%PROCESSOR_ARCHITECTURE%"=="x86" (
+    echo Downaloding goversioninfo 386...
+    curl -sSfL https://github.com/yuk7/goversioninfo/releases/download/v1.2.0-arm/goversioninfo_386.exe -o tools\goversioninfo.exe
+)
+if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+    echo Downaloding goversioninfo amd64...
+    curl -sSfL https://github.com/yuk7/goversioninfo/releases/download/v1.2.0-arm/goversioninfo_amd64.exe -o tools\goversioninfo.exe
+)
+if "%PROCESSOR_ARCHITECTURE%"=="ARM" (
+    curl -sSfL https://github.com/yuk7/goversioninfo/releases/download/v1.2.0-arm/goversioninfo_arm.exe -o tools\goversioninfo.exe
+)
+if "%PROCESSOR_ARCHITECTURE%"=="ARM64" (
+    curl -sSfL https://github.com/yuk7/goversioninfo/releases/download/v1.2.0-arm/goversioninfo_arm64.exe -o tools\goversioninfo.exe
+)
+
 exit /b
--- src/go.mod
+++ src/go.mod
@@ -6,7 +6,8 @@ require (
 	github.com/mitchellh/go-ps v1.0.0
 	github.com/satori/go.uuid v1.2.0
 	github.com/yosuke-furukawa/json5 v0.1.1
-	golang.org/x/sys v0.0.0-20210113181707-4bcb84eeeb78
+	golang.org/dl v0.0.0-20210713194856-38ddc79c2163 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c
 	golang.org/x/text v0.3.6
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
