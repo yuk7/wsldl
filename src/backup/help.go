4f4520a27ba982ed43afe7f2c34f3954a946f942
yuk7 <yukx00@gmail.com>
2024-05-06 15:16:47 +0900

    fix backup help message

--- src/backup/help.go
+++ src/backup/help.go
@@ -7,7 +7,7 @@ func ShowHelp(showTitle bool) {
 	}
 	println("    backup [contents]")
 	println("      - `--tar`: Output backup.tar to the current directory.")
-	println("      - `--tgz`: Output backup.tar.tar to the current directory.")
+	println("      - `--tgz`: Output backup.tar.gz to the current directory.")
 	println("      - `--vhdx`: Output backup.ext4.vhdx to the current directory. (WSL2 only)")
 	println("      - `--vhdxgz`: Output backup.ext4.vhdx.gz to the current directory. (WSL2 only)")
 	println("      - `--reg`: Output settings registry file to the current directory.")
