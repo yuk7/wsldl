cb09b8de00b2ca05931642bdb041b4253760ba22
guoguangwu <guoguangwug@gmail.com>
2024-05-14 13:44:00 +0800

    fix: typos
    Signed-off-by: guoguangwu <guoguangwug@gmail.com>

--- src/version/execute.go
+++ src/version/execute.go
@@ -5,7 +5,7 @@ import (
 	"runtime"
 )
 
-//Execute is default version entrypoint. prints version infomation
+//Execute is default version entrypoint. prints version information
 func Execute() {
 	fmt.Printf("%s, version %s  (%s)\n", project, version, runtime.GOARCH)
 	fmt.Printf("%s\n", url)
